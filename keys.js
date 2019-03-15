8794a62c43eebf0c57a0b260015b7c4952782102
Anthony Mastanduno <anthonymastanduno@anthonys-mbp.lan>
2019-03-14 22:08:55 -0400

    got bandsintown to work, need to figure out how to get more results

--- keys.js
+++ keys.js
@@ -1,5 +1,5 @@
 /* in CLI or command line interface if you type node keys.js, expect to see This is loaded */
-console.log('This is loaded')
+// console.log('This is loaded')
 
 exports.spotify = {
     id: process.env.SPOTIFY_ID,
--- liri.js
+++ liri.js
@@ -1,10 +1,84 @@
-// this will read and set any environment variables with the dotenv package
+// this shares the information in dotenv with the liri.js file and hides the keys
 require('dotenv').config();
 
-// this will import the keys.js file and store it in a variable
-let keys = require('./keys.js');
-console.log(keys);
+// GLOBAL VARIABLES
 
-// access keys information
+let keys = require('./keys.js');
+
+let fs = require('fs');
+let axios = require('axios')
+let moment = require('moment')
+
+let Spotify = require('node-spotify-api');
 let spotify = new Spotify(keys.spotify);
-console.log(spotify);
+
+let command = process.argv[2];
+
+let parameter = process.argv[3];
+
+function switchCase(command) {
+    console.log(command);
+    switch(command) {
+
+        case 'concert-this':
+            bandsInTown(parameter);
+            break;
+
+        case 'spotify-this-song':
+            spotifySong(parameter);
+            break;
+
+        case 'movie-this':
+            omdbMovie(parameter);
+            break;
+
+        case 'do-what-it-says':
+            getRandom();
+            break;
+
+    }
+};
+
+
+function bandsInTown(parameter) {
+
+axios.get("https://rest.bandsintown.com/artists/" + parameter + "/events?app_id=codingbootcamp").then (
+        function(response) {
+            // console.log(response)
+            console.log(response.data[0]);
+            console.log("VENUE: " + response.data[0].venue.name);
+            console.log("LOCATION: " + response.data[0].venue.city + ",",response.data[0].venue.region);
+            console.log("DATE: " + moment(response.data[0].datetime).format("MM/DD/YYYY"));
+           
+           
+        }
+    )
+};
+
+
+function spotifySong(parameter) {
+    
+    spotify.search ({type:'track', query: 'All the Small Things'}, function(err,data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+    console.log(data);
+    });
+
+};
+// spotifySong(parameter)
+
+
+function omdbMovie(parameter) {
+
+};
+
+function getRandom(parameter) {
+
+};
+
+
+switchCase(command);
+
+
+
